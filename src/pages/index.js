3f736de1becd74bef3239b6ebaca69b0e593caaf
Abdulgafur <bersugur.abdulgafur@gmail.com>
2020-08-05 13:00:25 +0600

    chore: remove unused imorts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import App from "../components/App";
 
 import CreateCollection from "../components/CreateCollection";
 import Collections from "../components/Collections";
-import { initializeApollo } from "../lib/apolloClient";
-import { ALL_COLLECTIONS } from "../gql/collections.api";
 
 const IndexPage = () => (
   <App>
